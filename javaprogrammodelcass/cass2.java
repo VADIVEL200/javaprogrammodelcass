9791e2e4f660605e5122b7710a7e599ea6609ce2
VADIVEL200 <KV2844205@gmail.com>
2025-06-30 20:05:22 +0530

    java program model cass 2 add

---
+++ javaprogrammodelcass/cass2.java
@@ -0,0 +1,11 @@
+class hello{
+    public static void main(String arge[])
+    {
+       int vadivel=67;
+       String arun="vadivel";
+       
+        System.out.print(vadivel);
+         System.out.print(arun);
+
+    }
+}
