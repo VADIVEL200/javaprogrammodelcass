f7049394a8278360800759d16b52b52886253608
VADIVEL200 <KV2844205@gmail.com>
2025-06-30 19:56:06 +0530

    java program model cass 1 add

---
+++ javaprogrammodelcass/cass1.java
@@ -0,0 +1,11 @@
+import java.lang.System;
+import java.util.Scanner;
+class hello{
+    public static void main(String arge[])
+    {
+        Scanner vadivel=new Scanner(System.in);
+        int a=vadivel.nextInt();
+        System.out.print(a);
+    }
+}
+
