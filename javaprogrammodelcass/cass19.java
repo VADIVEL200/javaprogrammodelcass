605ff99e606436800dd5d4abac02afec846df1e6
VADIVEL200 <KV2844205@gmail.com>
2025-07-02 20:59:17 +0530

    java program model cass 19 add

---
+++ javaprogrammodelcass/cass19.java
@@ -0,0 +1,20 @@
+import java.lang.System;
+import java.util.Scanner;
+class hello{
+    public static void main(String arge[])
+
+    {
+        for(int  i=0;i<=10;i++)
+        {
+            if(i%2==0)
+            {
+                //System.out.println("Even number:"+i);
+            }
+            else
+            {
+                System.out.println("odd  number:"+i); 
+            }
+    
+    }
+    }
+}
