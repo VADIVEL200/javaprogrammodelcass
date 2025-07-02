b93f5be96fc06e404800acd229fd1cedf7ad617b
VADIVEL200 <KV2844205@gmail.com>
2025-07-02 20:56:27 +0530

    java program model cass 17 add

---
+++ javaprogrammodelcass/cass17.java
@@ -0,0 +1,16 @@
+import java.lang.System;
+import java.util.Scanner;
+class hello{
+    public static void main(String arge[])
+
+    {
+         Scanner vadivel=new Scanner(System.in);
+         System.out.println("enter the a value");
+        int a=vadivel.nextInt();
+        System.out.println("Enter the b value");
+        int b= vadivel.nextInt();
+        for(int  i=a;i<=b;i++)
+        {
+    System.out.println(i);
+    }}
+}
